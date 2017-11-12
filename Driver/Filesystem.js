0d03aa1fdcd1894a32fc737e13c9ca38f35a4f60
Yme-Jan Iedema <yme-jan@iedema.me>
2017-11-12 15:57:28 +0100

    Basic CRUD implementation for the Filesystem Driver

--- Driver/Filesystem.js
+++ Driver/Filesystem.js
@@ -1,6 +1,7 @@
 const Driver = require('@wardrobe/databasebundle').Driver;
-let Engine   = require('tingodb')();
-const fs     = require('fs-extra');
+
+const fs     = require('fs-extra'),
+      Engine = require('tingodb')({nativeObjectID: true});
 
 class Filesystem extends Driver
 {
@@ -12,29 +13,50 @@ class Filesystem extends Driver
         fs.ensureDirSync(database_directory);
 
         this.db = new Engine.Db(database_directory, {});
-
-        // cache all schemas
-        this.schemas = {};
     }
 
-    find (model, id)
+    async find (model, id)
     {
-        console.log(model, id);
+        return await this.findOneBy(model, schema.getPrimary(), id);
     }
 
-    findAll ()
+    async findAll (model)
     {
-
+        return await this.findBy(model, undefined, undefined);
     }
 
-    findBy ()
+    async findOneBy (model, by, value)
     {
-
+        return (await this.findBy(model, by, value))[0] || null;
     }
 
-    findOneBy ()
+    async findBy (model, by, value)
     {
+        let schema     = model.getSchema();
+        let collection = this.db.collection(`${schema.getName()}.db`);
+        let operation  = {};
 
+        if (by && value) {
+            operation[this._resolveBy(schema, by)] = value;
+        }
+
+        return await new Promise((resolve, reject) => {
+            collection.find(operation).toArray((err, r) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(r.map(o => {
+                    let entity = this._objectToEntity(model, o);
+
+                    Object.defineProperty(entity, '__hydrated__', {
+                        writable:   false,
+                        enumerable: false,
+                        value:      true
+                    });
+                    return entity;
+                }));
+            });
+        });
     }
 
     count ()
@@ -42,11 +64,99 @@ class Filesystem extends Driver
 
     }
 
-    async save (obj, test)
+    async save (model, entity)
     {
-        let collection = this.db.collection(`${obj.name}.db`);
+        let schema     = model.getSchema();
+        let collection = this.db.collection(`${schema.getName()}.db`);
 
-        return await collection.insert(test);
+        Object.keys(schema).forEach(async (name) => {
+            let nameKey   = {};
+            nameKey[name] = 1;
+            collection.createIndex(nameKey, {
+                unique: schema[name].unique
+            });
+        });
+
+        if (typeof entity.__hydrated__ !== 'undefined') {
+            return await new Promise((resolve, reject) => {
+                let find = {};
+
+                find[schema.getPrimary()] = entity[schema.getPrimary(true)];
+
+                collection.update(find, this._entityToObject(entity), (err, r) => {
+                    if (err) {
+                        return reject(err);
+                    }
+                    resolve(r);
+                });
+            });
+        }
+
+        entity[schema.getPrimary(true)] = new Engine.ObjectID(); // new ObjectID?
+
+        return await new Promise((resolve, reject) => {
+            collection.insert(this._entityToObject(entity), (err, r) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                entity[schema.getPrimary(true)] = r[0]._id;
+
+                resolve();
+            });
+        });
+    }
+
+    async remove (model, entity)
+    {
+        let schema     = model.getSchema();
+        let collection = this.db.collection(`${schema.getName()}.db`);
+
+        let operation = {};
+
+        operation[schema.getPrimary()] = entity[schema.getPrimary(true)];
+
+        return await collection.remove(operation);
+    }
+
+    _entityToObject (entity)
+    {
+        let schema = entity.constructor.getSchema();
+        let result = {};
+
+        Object.keys(schema).forEach(key => {
+            if (entity.hasOwnProperty(schema[key].key)) {
+                result[key] = entity[schema[key].key];
+            }
+        });
+
+        return result;
+    }
+
+    _objectToEntity (model, object)
+    {
+        let schema = model.getSchema();
+        let entity = new model();
+        for (let attr in object) {
+            if (object.hasOwnProperty(attr)) {
+                if (typeof schema[attr] !== 'undefined') {
+                    entity[schema[attr].key] = object[attr];
+                }
+            }
+        }
+
+        return entity;
+    }
+
+    _resolveBy (schema, by)
+    {
+        for (let s in schema) {
+            if (schema.hasOwnProperty(s)) {
+                if (schema[s].key === by) {
+                    return s;
+                }
+            }
+        }
     }
 
 }
